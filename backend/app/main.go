b88f34fde48ef3c68cee4b7407da7667593f3be3
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-08-09 13:58:15 +0300

    Included the favicon and added my custom made handler library.

--- backend/app/main.go
+++ backend/app/main.go
@@ -4,15 +4,23 @@ import (
 	"os"
 	"fmt"
 	"net/http"
-	"github.com/a-h/templ"
 	"PulpuVOX/pages/home"
+	"github.com/a-h/templ"
+	"github.com/jackc/pgx/v5"
+	"github.com/gchalakovmmi/handlers"
 )
 
 func main() {
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "favicon.ico")
+	})
 	http.Handle("/", http.RedirectHandler("/home", http.StatusSeeOther))
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		templ.Handler(home.Home()).ServeHTTP(w, r)
 	})
+	http.HandleFunc("/test", handlers.WithDB(func(w http.ResponseWriter, r *http.Request, conn *pgx.Conn){
+		templ.Handler(home.Home()).ServeHTTP(w, r)
+	}))
 
 	port := os.Getenv("BACKEND_PORT")
 	fmt.Printf("Serving on port %s ...\n", port)
