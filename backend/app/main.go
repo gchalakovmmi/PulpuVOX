ebf072d445b99f17d308f498bd48df409e66e389
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-08-09 23:29:55 +0300

    Added connecting to the database with a struct. Added a commented version of a sample query to make it easier to write real queries.

--- backend/app/main.go
+++ backend/app/main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"os"
 	"fmt"
+	"strconv"
 	"net/http"
 	"PulpuVOX/pages/home"
 	"github.com/a-h/templ"
-	"github.com/jackc/pgx/v5"
+	// "github.com/jackc/pgx/v5"
 	"github.com/gchalakovmmi/handlers"
+	// "time"
+	// "context"
 )
 
 func main() {
+	dbConnectionDetails := handlers.ConnectionDetails{
+		User: 		os.Getenv("POSTGRES_USER"),
+		Password:	os.Getenv("POSTGRES_PASSWORD"),
+		ServerIP:	os.Getenv("POSTGRES_CONTAINER_NAME"),
+		Schema:		os.Getenv("POSTGRES_DB"),
+	}
+	var err error
+	dbConnectionDetails.Port, err = strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	if err != nil {
+		panic(fmt.Sprintf("Invalid POSTGRES_PORT: %v", err))
+	}
+
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "favicon.ico")
 	})
@@ -18,9 +33,16 @@ func main() {
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		templ.Handler(home.Home()).ServeHTTP(w, r)
 	})
-	http.HandleFunc("/test", handlers.WithDB(func(w http.ResponseWriter, r *http.Request, conn *pgx.Conn){
-		templ.Handler(home.Home()).ServeHTTP(w, r)
-	}))
+	// http.HandleFunc("/test", handlers.WithDB(dbConnectionDetails, func(w http.ResponseWriter, r *http.Request, conn *pgx.Conn){
+	// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	//    defer cancel()
+	// 	var fld string
+	// 	err := conn.QueryRow(ctx, "select fld from tbl").Scan(&fld)
+	// 	if err != nil {
+	// 	}
+	// 	fmt.Println(fld)
+	// 	templ.Handler(home.Home()).ServeHTTP(w, r)
+	// }))
 
 	port := os.Getenv("BACKEND_PORT")
 	fmt.Printf("Serving on port %s ...\n", port)
